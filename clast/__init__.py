00e865178f8e1762e7cd1ec8d44713d73cc58c47
Andrew Walker <walker.ab@gmail.com>
2016-07-04 12:08:59 +0000

    tidy up of DynTypedNode in python

--- clast/__init__.py
+++ clast/__init__.py
@@ -1,25 +1,8 @@
 import _clast
 from _clast import *
 
-## REPRESENTATIVE CLASSES ONLY
+def __get(self, kind):
+    return getattr(self, '_get_' + kind.__name__)()
 
-def cxxRecordDecl(*args):
-    return _clast._cxxRecordDecl(list(args))
-
-def decl(*args):
-    return _clast._decl(list(args))
-
-def stmt(*args):
-    return _clast._stmt(list(args))
-
-def forStmt(*args):
-    return _clast._forStmt(list(args))
-
-def hasLoopInit(arg):
-    return _clast._hasLoopInit(arg)
-
-def ifStmt(*args):
-    return _clast._ifStmt(list(args))
-
-def hasCondition(expr):
-    return _clast._hasCondition(expr)
+# Monkey patch an extra method on that we can't do in C++
+_clast.DynTypedNode.get = __get
